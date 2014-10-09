0872b498993564be5ffe23f9fb6c413b17ba5b8f
Ludovic <ludovic.deparis@time-reversal-communications.fr>
2014-10-09 16:19:32 +0200

    Mise à jour POST et DELETE d'un risque

--- routes/risks.js
+++ routes/risks.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var Risque = require('../model/risque');
 
 router.use(function(req,res,next){
-	console.log(req.method,req.url);
+	//console.log(req.method,req.url);
 	next();
 })
 
@@ -13,6 +13,7 @@ router.use(function(req,res,next){
 router.get('/', function(req, res) {
   res.send('Risks management');
 });
+
 router.route('/risk')
   .get(function(req,res,next){
   	Risque.find(function(err, risque) {
@@ -24,20 +25,38 @@ router.route('/risk')
   })
   .post(function(req,res,next){
     var risque= new Risque();
-    risque.description=req.body.description; 
-    console.log(risque.description)   ;
-    risque.save(function(err) {
+    risque.description=req.body.description;   
+    risque.type=req.body.type;
+    risque.risk_opp =req.body.risk_opp;
+    risque.date_created=req.body.date_created;
+    risque.origine=req.body.origine;
+    risque.gravity=req.body.gravity;
+    risque.probability=req.body.probability;
+    risque.impact=req.body.impact;
+    risque.impact_desc=req.body.impact_desc;
+    risque.Status_open=req.body.Status_open;
+    risque.preventive_action=req.body.preventive_action;
+    risque.Leader=req.body.Leader;
+
+    console.log("description= "+ risque.description)   ;
+
+    risque.save(function(err,risk_just_saved) {
       if (err)
         res.send(err);
-
-      res.json({ message: 'risque created!' });
+      else
+        //on cherche le risque dans la bdd pour retourner son entrée au client
+        Risque.findById(risk_just_saved, function(err, risque) {
+          if (err)
+            res.send(err);
+          res.json(risque);
+        });
+        //res.json({ message: 'risque created!' });
     });
     //res.send('création du risque dans la bdd' + risque.description);
   })
 
 router.route('/risk/:risk_id')
-
-  // get the risqye with that id (accessed at GET http://localhost:3000/risks/risk/:risque_id)
+  // get the risque with that id (accessed at GET http://localhost:3000/risks/risk/:risque_id)
   .get(function(req, res) {
     Risque.findById(req.params.risk_id, function(err, risque) {
       if (err)
@@ -45,9 +64,9 @@ router.route('/risk/:risk_id')
       res.json(risque);
     });
   })
+
   // update the risque with this id (accessed at PUT http://localhost:3000/risks/risk/:risque_id)
   .put(function(req, res) {
-
     // use our risque model to find the risque we want
     Risque.findById(req.params.risk_id, function(err, risque) {
 
@@ -66,6 +85,7 @@ router.route('/risk/:risk_id')
       };
     });
   })
+
   // delete the risque with this id (accessed at DELETE http://localhost:3000/risks/risk/:risk_id)
   .delete(function(req, res) {
     Risque.remove({
