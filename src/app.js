cfef27e667a4aef9d17883915481e98a82084f5e
Ludovic <ludovic.deparis@time-reversal-communications.fr>
2014-10-23 08:24:25 +0200

    message d'erreur publié seulement si NODE_ENV=development

--- src/app.js
+++ src/app.js
@@ -91,7 +91,7 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: err
+        error: {}
     });
 });
 
